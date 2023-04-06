0750745e7f854bdf317f43c67c12096660a55ef5
Sandun Induranga <sandutharu40@gmail.com>
2023-04-06 12:50:43 +0530

    Updated Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header/Header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import { AppBar, Box, Toolbar } from "@mui/material";
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Box>box</Box>
+        <Box>box</Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
