93e06b0a02d43b0a861d3bf4c6224ae3ae43bc0b
Sandun Induranga <sandutharu40@gmail.com>
2023-04-08 00:38:37 +0530

    Home Button

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,12 +1,28 @@
-import { Box, Paper, Skeleton, Stack } from "@mui/material";
+import { Home } from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+} from "@mui/material";
 
 const Menu = () => {
   return (
     <Box>
       <Paper elevation={1} sx={{ width: 1 / 5, minHeight: "100vh" }}>
-        <Stack spacing={2}>
-          <Skeleton variant="text" height={100} />
-        </Stack>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </Paper>
     </Box>
   );
