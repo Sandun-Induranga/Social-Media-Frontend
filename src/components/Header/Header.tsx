a61d72a507e8e9467d716f6173786f4e7672b95a
Sandun Induranga <sandutharu40@gmail.com>
2023-04-07 23:36:16 +0530

    Navbar Updated

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Badge, Box, Toolbar } from "@mui/material";
+import { AppBar, Avatar, Badge, Box, Toolbar } from "@mui/material";
 import Diversity3Icon from "@mui/icons-material/Diversity3";
 import MailIcon from "@mui/icons-material/Mail";
 import { Notifications } from "@mui/icons-material";
@@ -10,13 +10,14 @@ const Header = () => {
         <Box>
           <Diversity3Icon />
         </Box>
-        <Box sx={{ display: "flex", gap: 2 }}>
+        <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
           <Badge badgeContent={1} color="error">
             <MailIcon sx={{ color: "white" }} />
           </Badge>
           <Badge badgeContent={4} color="error">
             <Notifications sx={{ color: "white" }} />
           </Badge>
+          <Avatar sx={{ ml: 2 }} alt="User" src="/static/images/avatar/2.jpg" />
         </Box>
       </Toolbar>
     </AppBar>
