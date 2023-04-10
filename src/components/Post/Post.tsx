c924c5ff02dc60a8c51f1d30a81ebe32340dbfa7
Sandun Induranga <sandutharu40@gmail.com>
2023-04-10 21:07:51 +0530

    Content Updated on Post

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,12 +1,32 @@
-import { BorderColor } from "@mui/icons-material";
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
 
 const Post = () => {
   return (
     <Box>
       <Paper sx={{ p: 2 }}>
-        <Grid>
-          <Typography variant="h6">John Cena</Typography>
+        <Grid container sx={{ justifyContent: "center" }}>
+          <Grid
+            item
+            sx={{
+              width: 5 / 6,
+              justifyContent: "space-between",
+              alignItems: "center",
+              gap: 2,
+            }}
+          >
+            <Grid item xs={12}>
+              <Avatar alt="User" src="/static/images/avatar/2.jpg" />
+              <Typography variant="caption">John Cena</Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid item xs={12}>
+              <Typography variant="caption">2023-04-10</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="caption">8.30 PM</Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Paper>
     </Box>
