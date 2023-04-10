000da16b72e2fb53758166cf475c8c8a13514285
Sandun Induranga <sandutharu40@gmail.com>
2023-04-10 20:52:01 +0530

    Content Updated on Post

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,9 +1,12 @@
 import { Box, Paper } from "@mui/material";
+import Post from "../Post/Post";
 
 const Feed = () => {
   return (
     <Box>
-      <Paper elevation={1} sx={{ minHeight: "100vh" }}></Paper>
+      <Paper elevation={1} sx={{ minHeight: "100vh", p: 6 }}>
+        <Post />
+      </Paper>
     </Box>
   );
 };
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,5 +1,16 @@
+import { BorderColor } from "@mui/icons-material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
+
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <Box>
+      <Paper sx={{ p: 2 }}>
+        <Grid>
+          <Typography variant="h6">John Cena</Typography>
+        </Grid>
+      </Paper>
+    </Box>
+  );
 };
 
 export default Post;
