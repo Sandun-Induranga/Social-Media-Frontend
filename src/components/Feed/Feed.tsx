a151e49b59874980cd62e27316b684d49896ebd5
Sandun Induranga <sandutharu40@gmail.com>
2023-04-16 21:31:57 +0530

    Content Updated

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -5,9 +5,9 @@ const Feed = () => {
   return (
     <Box>
       <Paper elevation={1} sx={{ minHeight: "100vh", py: 6, px: 20 }}>
-        <Post name="" date="" time="" />
-        <Post name="" date="" time="" />
-        <Post name="" date="" time="" />
+        <Post name="Dasun Dasun" date="2023-04-16" time="06:50 PM" />
+        <Post name="John Cena" date="2023-04-15" time="02:37 PM" />
+        <Post name="Max Max" date="2023-04-15" time="07:30 AM" />
       </Paper>
     </Box>
   );
