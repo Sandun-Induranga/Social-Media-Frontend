24fdba9a31a0ac52b52b40f6f21e133b0b304f3d
Sandun Induranga <sandutharu40@gmail.com>
2023-04-16 22:07:43 +0530

    Message Component Updated

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,10 +1,15 @@
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Button, Card, Typography } from "@mui/material";
+import game from "../../assets/game.jpg";
+import game2 from "../../assets/game2.jpg";
 
 const Message = () => {
   return (
-    <Box>
-      <Card>
+    <Box sx={{ p: 6 }}>
+      <Card elevation={1}>
         <Typography variant="h6">Games</Typography>
+        <img src={game} alt="" width={"100%"} />
+        <img src={game2} alt="" width={"100%"} />
+        <Button>View More</Button>
       </Card>
     </Box>
   );
